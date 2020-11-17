81f44412325431ccaca9cf5246db63c717d1ffa7
Brandon-Harris <brandonharris177@gmail.com>
2020-11-16 17:01:45 -0700

    logger added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
 import { searchRobots } from './reducers';
+import { createLogger } from "redux-logger"
+
+const logger = createLogger();
 
 const rootReducers = combineReducers({ searchRobots})
 
-const store = createStore(rootReducers)
+const store = createStore(rootReducers, applyMiddleware(logger))
 
 ReactDOM.render(
     <Provider store = {store}> 
